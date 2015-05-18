5308bcf2508f21e14a6caef44997974da40139d1
walkrain <walkrain@126.com>
2015-05-18 07:27:41 -0700

    add python lib case

---
+++ lib/assert.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Filename:assert.py
+mylist = ['item']
+assert len(mylist) >= 1
+print mylist
+mylist.pop()
+print mylist
+
+assert len(mylist) >= 1
+
---
+++ lib/exec.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# Filename:exec.py
+exec 'print "hello world"'
+
+print eval('2*3')
